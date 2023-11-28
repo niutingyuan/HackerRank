2acbbe9e8bb84cfcc90c4c08bdf7af2840c404f2
Thomas Niu <thomasniu@gmail.com>
2023-11-28 11:46:59 +0800

    adds left rotation operation to an array

---
+++ Arrays/LeftRotation/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rotLeft(a []int32, d int32) []int32 {
+
+}
+
+func main() {
+	// Sample Input
+	d := int32(4)
+	arr := []int32{1, 2, 3, 4, 5}
+
+	// Function call
+	result := rotLeft(arr, d)
+
+	// Print the result
+}
