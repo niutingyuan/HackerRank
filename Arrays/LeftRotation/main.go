414c998883ac4d23f690efc832aa073bf3b7a394
Thomas Niu <thomasniu@gmail.com>
2023-11-28 14:09:40 +0800

    Adds rotLeft() to perform left rotation operation on array.

--- Arrays/LeftRotation/main.go
+++ Arrays/LeftRotation/main.go
@@ -1,11 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func rotLeft(a []int32, d int32) []int32 {
+	n := int32(len(a))
 
+	// Calculate the effective number of rotations by taking the modulo of d with the array length
+	rotations := d % n
+
+	// Create a new slice to store the rotated array
+	rotatedArray := make([]int32, n)
+
+	// Perform the left rotation
+	for i := int32(0); i < n; i++ {
+		// Calculate the new index after rotation
+		newIndex := (i - rotations + n) % n
+		rotatedArray[newIndex] = a[i]
+	}
+
+	return rotatedArray
 }
 
 func main() {
@@ -16,5 +29,9 @@ func main() {
 	// Function call
 	result := rotLeft(arr, d)
 
-	// Print the result
+	// Print the result as a single line of space-separated integers
+	for _, num := range result {
+		fmt.Printf("%d ", num)
+	}
+
 }
